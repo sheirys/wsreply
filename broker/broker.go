6ccd393e562b1be23e23df7124a1a0cb2737f066
Tomas Šeirys <sheirys2@gmail.com>
2019-02-08 15:26:17 +0200

    Comments fix

--- broker/broker.go
+++ broker/broker.go
@@ -8,28 +8,28 @@ import (
 )
 
 // Broker defines publish/subscribe system. In general this is simple fan
-// out system but with some rules. When new sub
+// out system but with some additional rules.
 type Broker interface {
 
 	// Start should initialize all variables required by broker. This
-	// should be called before usage of other broker functions. When
-	// context dies, all subscribers should be unsubscribed so
-	// publishers know that there is nothing left.
+	// should be called before use of other broker functions. When context
+	// dies, all subscribers should be unsubscribed so publishers know that
+	// there is nothing left.
 	Start(context.Context, *sync.WaitGroup) error
 
 	// Broadcast should send message to all subscribers in broker.
 	Broadcast(Message) error
 
-	// NewPublisher should return new publishers stream, where messages
-	// produced by broker should appear e.g. in this stream notifications
-	// about new subcscriber connection or if there is no other
-	// subscribers left should be pushed.
+	// AttachPublisherStream should attach publisher stream to provided
+	// websocket connection. Here messages about subscriber connections
+	// should be streamed.
 	AttachPublisherStream(*websocket.Conn) error
 
-	// NewSubscriberStream should return new subscribers stream where
-	// messages pushed by publishers should appear.
+	// AttachSubscriberStream should attach subscribers stream to provided
+	// websocket connection. Here messages received from publishers should
+	// be streamed.
 	AttachSubscriberStream(*websocket.Conn) error
 
-	// Unsubscribe should close passed stream.
+	// Deattach this websocket connection from all streams.
 	Deattach(*websocket.Conn) error
 }
